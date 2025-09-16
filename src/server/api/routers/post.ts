f3441c6b60ef25570cc2eb482d195160ba7f49c0
Jorge Raul Ochoa-Reyes <jorgeochoareyes@gmail.com>
2025-09-16 01:41:04 -0700

    build fix

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -2,38 +2,16 @@ import { z } from "zod";
 
 import {
   createTRPCRouter,
-  protectedProcedure,
-  publicProcedure,
-} from "~/server/api/trpc";
-import { posts } from "~/server/db/schema";
+  protectedProcedure, 
+} from "~/server/api/trpc"; 
 
 export const postRouter = createTRPCRouter({
-  hello: publicProcedure
+  hello: protectedProcedure
     .input(z.object({ text: z.string() }))
     .query(({ input }) => {
       return {
         greeting: `Hello ${input.text}`,
       };
     }),
-
-  create: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      await ctx.db.insert(posts).values({
-        name: input.name,
-        createdById: ctx.session.user.id,
-      });
-    }),
-
-  getLatest: protectedProcedure.query(async ({ ctx }) => {
-    const post = await ctx.db.query.posts.findFirst({
-      orderBy: (posts, { desc }) => [desc(posts.createdAt)],
-    });
-
-    return post ?? null;
-  }),
-
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
-  }),
+ 
 });
